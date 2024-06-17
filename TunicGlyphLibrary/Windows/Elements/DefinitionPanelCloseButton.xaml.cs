b7a4dc9d1b1c784c20e96a320efcb03a6e14b2f5
PixelHole <66640043+PixelHole@users.noreply.github.com>
2024-06-17 11:55:57 +0330

    updated visuals

---
+++ TunicGlyphLibrary/Windows/Elements/DefinitionPanelCloseButton.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace TunicGlyphLibrary.Windows.Elements
+{
+    public partial class DefinitionPanelCloseButton : UserControl
+    {
+        public DefinitionPanelCloseButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
