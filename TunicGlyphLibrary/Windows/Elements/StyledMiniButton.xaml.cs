4f3bf05962a33d13e1b393bb99984dec29b84b9c
PixelHole <66640043+PixelHole@users.noreply.github.com>
2024-06-18 14:53:08 +0330

    first mvp

---
+++ TunicGlyphLibrary/Windows/Elements/StyledMiniButton.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace TunicGlyphLibrary.Windows.Elements
+{
+    public partial class StyledMiniButton : UserControl
+    {
+        public StyledMiniButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TunicGlyphLibrary/Windows/Elements/StyledScrollBar.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace TunicGlyphLibrary.Windows.Elements
+{
+    public partial class StyledScrollBar : UserControl
+    {
+        public StyledScrollBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
